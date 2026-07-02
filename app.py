e2eb2b77e5e058626d674c58cee4d166ce3d202f
Sai Kiran Latchupatni <lsaikiran2804@gmail.com>
2026-07-02 09:09:09 +0530

    Changed source code

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps!"
-
-def home2():
-    return "Practice and Learning!"
+    return "Executed CICD Pipeline and Deployment!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
